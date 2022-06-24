b4a99ce5c0ba027b644926cb2fad32c59f8c7fc9
ds02168 <1995musso@gmail.com>
2022-06-24 23:48:59 +0900

    2022-06-24

---
+++ section3-week1/src/main/java/com/codestates/coffee/CoffeeController.java
@@ -0,0 +1,54 @@
+package com.codestates.coffee;
+
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/v1/coffees")
+public class CoffeeController {
+    @PostMapping
+    public ResponseEntity postCoffee(@RequestBody CoffeePostDto coffeePostDto) {
+        return new ResponseEntity(coffeePostDto,HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{coffee-id}")
+    public ResponseEntity patchCoffee(@PathVariable("coffee-id") long coffeeId,
+                                      @RequestBody CoffeePatchDto coffeePatchDto) {
+
+        coffeePatchDto.setCoffeeId(coffeeId);
+
+        return new ResponseEntity(coffeePatchDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/{coffee-id}")
+    public ResponseEntity getCoffee(@PathVariable("coffee-id") long coffeeId) {
+        System.out.println("# coffeeId: " + coffeeId);
+
+        // not implementation
+
+        return new ResponseEntity<Map>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity getCoffees() {
+        System.out.println("# get Coffees");
+
+        // not implementation
+
+        return new ResponseEntity<Map>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{coffee-id}")
+    public ResponseEntity deleteCoffee(@PathVariable("coffee-id") long coffeeId) {
+        // No need business logic
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ section3-week1/src/main/java/com/codestates/coffee/CoffeePostDto.java
@@ -0,0 +1,31 @@
+package com.codestates.coffee;
+
+public class CoffeePostDto {
+    private String engName;
+    private String korName;
+    private int price;
+
+    public String getEngName() {
+        return engName;
+    }
+
+    public void setEngName(String engName) {
+        this.engName = engName;
+    }
+
+    public String getKorName() {
+        return korName;
+    }
+
+    public void setKorName(String korName) {
+        this.korName = korName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
