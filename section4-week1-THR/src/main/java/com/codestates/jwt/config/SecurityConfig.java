d25fef309a0bad0006376b44c787a7c2475cad2e
ds02168 <1995musso@gmail.com>
2022-07-28 23:07:09 +0900

    2022-07-28

---
+++ section4-week1-THR/src/main/java/com/codestates/jwt/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.codestates.jwt.config;
+
+import com.codestates.jwt.filter.FirstFilter;
+import com.codestates.jwt.filter.JwtAuthenticationFilter;
+import com.codestates.jwt.filter.JwtAuthorizationFilter;
+import com.codestates.jwt.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final CorsFilter corsFilter;
+    private final MemberRepository memberRepository;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+//        http.addFilterBefore(new FirstFilter(), BasicAuthenticationFilter.class);
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .apply(new CustomDsl())
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/v1/user/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+
+    public class CustomDsl extends AbstractHttpConfigurer<CustomDsl,HttpSecurity>{
+        public void configure(HttpSecurity builder) throws Exception{
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+            builder
+                    .addFilter(corsFilter)
+                    .addFilter(new JwtAuthenticationFilter(authenticationManager))
+                    .addFilter(new JwtAuthorizationFilter(authenticationManager,memberRepository));
+        }
+    }
+}
