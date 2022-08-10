50ddebe623465722e0a6ecb1257c10d590de280b
ds02168 <1995musso@gmail.com>
2022-08-10 19:58:50 +0900

    2022-08-10

---
+++ section4-week3-WED/src/main/java/com/codestates/Section4week3ReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.codestates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section4week3ReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section4week3ReactiveApplication.class, args);
+	}
+
+}
---
+++ section4-week3-WED/src/test/java/com/codestates/section4week3Reactive/Section4week3ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.section4week3Reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section4week3ReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
